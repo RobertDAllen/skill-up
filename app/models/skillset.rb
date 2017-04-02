51f59da66e600b1c4efe4d52d4ad15bcb7c961e0
Robert Allen <contact@robertdallen.com>
2017-04-02 17:54:07 +0000

    Set Total Points on Index

--- app/models/skillset.rb
+++ app/models/skillset.rb
@@ -1,4 +1,12 @@
 class Skillset < ActiveRecord::Base
     has_many :workouts
     validates_presence_of :name
+    def total_points
+        sum = 0  
+        workouts.each do |w|
+            sum += w.points
+        end
+        return sum
+
+    end
 end
