711330d991e507f2aefcf3864799169a890efb04
Robert Allen <contact@robertdallen.com>
2017-04-02 18:06:11 +0000

    total points works better now yay woo omg

--- app/models/skillset.rb
+++ app/models/skillset.rb
@@ -2,11 +2,7 @@ class Skillset < ActiveRecord::Base
     has_many :workouts
     validates_presence_of :name
     def total_points
-        sum = 0  
-        workouts.each do |w|
-            sum += w.points
-        end
-        return sum
-
+        return workouts.sum("points")
     end
+    
 end
