de26f45c6916aa9c5684a0199bdecc109181de9e
Robert Allen <contact@robertdallen.com>
2017-07-24 23:09:35 +0000

    Validations added to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
     has_many :workouts
     has_many :skillsets
+    
+    validates :username, presence: true, 
+            uniqueness: {case_sensitive: false}, 
+            length: { in: 5...25 }
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, 
+            uniqueness: {case_sensitive: false}, 
+            format: {with: VALID_EMAIL_REGEX },
+            length: { maximum: 100 }
+    validates :name, presence: true
 end
